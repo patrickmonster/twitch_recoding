f43c3eee64c71fe15cf16314e0c56afca87b275a
Soung jin Ryu <rsj11201@gmail.com>
2019-11-11 18:01:14 +0900

    명령 해석 방식 변경(안정화)
    change message parse 
    명령 규칙 변경(기존 에러처리 무시하는것 처리)
    명령 규칙 추가 => !카운트 <카운트이름>
    
    자동으로 카운팅 하여 데이터를 처리함.

--- chatbot.py
+++ chatbot.py
@@ -1,104 +1,115 @@
-import sys
+#!/usr/bin/env python
+import os
+import json
 import websocket
 import requests
 
-channel = ''#chat room name
-username = '' # bot user name
-passwd = '' # oauth: bot passwd
-comm = "!" # call bot command
+
+channel = '#' # 접속 채팅방 이름 >#userid
+username = ''#챗봇 클라이언트 id
+passwd = 'oauth:'#리다이렉션 url
+comm = "!" #봇 명령 인식 기호
 
 def on_message(ws, message):
-    if message == "":# no message
+    if message == "":
         return
-    try:
-        data = parse_message(message.replace('\r\n',''))
-        if data['command'] == "PRIVMSG":# recive to message
-            print(data['user']['display-name']+" :",data['message'])
-            if data['message'][0] == comm:
-                get_command(ws,data['user'],data['message'][1:])
-            else:
-                get_message(ws,data['user'],data['message'])
-        elif data['command'] == "PING":# sending Ping to pong
-            on_send(ws,data['message'],"PONG")
-        elif data['command'] == "JOIN":# join to user
-            print(data['message'])
-    except Exception as e:# non error
-        print("Error ",message, e)
+    #try:
+    data = parse_message(message.replace('\r\n',''))
+    if data['command'] == "PRIVMSG":
+        if data['message'][0] == comm:
+            get_command(ws,data,data['message'][1:])
+        else:
+            get_message(ws,data,data['message'])
+    elif data['command'] == "PING":
+        on_send(ws,data['message'],"PONG")
+    elif data['command'] == "JOIN":
+        print(data['message'])
 
 def on_error(ws, error):
-    print("ERROR", error)#service connect error
+    print("ERROR", error)
 
-def on_close(ws):# server is close
-    ws.closed()
+def on_close(ws):
+    try:
+        ws.closed()
+    except:
+        pass
     print("### closed ###")
 
-def send_message(ws, message):# send to message
-    on_send(ws,channel + " :" +message)
+def send_message(ws, message):
+    on_send(ws,channel + " :" +str(message))
 
-def on_send(ws,message ,option ='PRIVMSG'):# send command
-    print("전송 : %s %s" %(option,message))
+def on_send(ws,message ,option ='PRIVMSG'):
+    #print("전송 : %s %s" %(option,message))
     ws.send(option + " " + message)
 
-def on_open(ws):# connect twitch server to first sending data
+def on_open(ws):
     on_send(ws,"REQ :twitch.tv/tags twitch.tv/commands twitch.tv/membership","CAP")
-    on_send(ws,passwd,"PASS")# pw send
-    on_send(ws,username,"NICK")# send bot name
-    on_send(ws,channel,"JOIN")# send join call
+    on_send(ws,passwd,"PASS")
+    on_send(ws,username,"NICK")
+    on_send(ws,channel,"JOIN")
 
-def parse_message(rawMessage):# convert to message
+def parse_message(rawMessage):
     parseMessage = {'message':'','command':''}
-    if (rawMessage[0] == ':'):#command to protocal
+    if (rawMessage[0] == ':'):
         data = rawMessage.split()
         parseMessage['command'] = data[1]
         if (parseMessage['command'] == 'JOIN'):
             parseMessage['message'] = "Join user :" + data[2]
         else :
             parseMessage['message'] = rawMessage
-    elif rawMessage[:4] in ["PING"]:# recive to ping protocal
+    elif rawMessage[:4] in ["PING"]:
         parseMessage['command'],parseMessage['message'] = rawMessage.split()
-    else:# recive data
-        data = rawMessage.split(":")
-        parseMessage['command'] = data[1].split()[1]
-        parseMessage['message'] = ":".join(data[2:])
-        data = data[0].split(';')
-        parseMessage['user'] = {}
+    else:
+        #try: #129955642
+        data = rawMessage.split(";")
         for i in data:
-            item = i.split('=')
-            parseMessage['user'][item[0]] = item[1]
+            d = i.split("=")
+            if i == '' or len(d) < 2:
+                continue
+            parseMessage[d[0]] = d[1]
+        if 'user-type' in parseMessage:
+            user_type = parseMessage['user-type'].split()
+            if "mod" in user_type[0]: #타 봇/관리자 
+                del user_type[0]
+            parseMessage['user-connect'] = user_type[0]
+            parseMessage['command'] = user_type[1]
+            parseMessage['chatroom'] = user_type[2]
+            parseMessage['message'] = " ".join(user_type[3:])[1:]
     return parseMessage
 
 countl = {}
 
-def get_command(ws,data,message):#command call
-    message = message.split()
-    if message[0] == "생존":#a live command 
-        send_message(ws,"테스트봇 실행중...")
+def get_command(ws,data,message):
+    if len(message) == 0:
         return
-    elif message[0] == "카운트":# count command
+    message = message.split()
+    #print(message)
+    if message[0] == "생존":
+        send_message(ws,"Recoding bot 실행중...")
+    elif message[0] == "카운트":
         if len(message) <= 1:
-            send_message(ws, "명령어 에러! \r\n!카운트 <카운트이름>")
+            send_message(ws, "명령에러] :!카운트 <카운트이름>")
             return
         if message[1] not in countl:
             countl[message[1]] = 0
             send_message(ws, "항목 추가 "+ message[1])
-            return
         else:
             send_message(ws,"현재 카운트 :" + str(countl[message[1]]))
-            return
     else:
         #send_message(ws,"명령어 추가중..")
         return
-def get_message(ws,data,message):#message call
+def get_message(ws,data,message):
+    print(data['display-name']+" :",data['message'])
     for i in countl:
         if i in message:
             countl[i] += 1
             break
 
 if __name__ == "__main__":
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("wss://irc-ws.chat.twitch.tv:443/",#subprotocols="irc",
+    #websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("wss://irc-ws.chat.twitch.tv:443/",
+                            on_open = on_open,
                             on_message = on_message,
                             on_error = on_error,
                             on_close = on_close)
-    ws.on_open = on_open
     ws.run_forever()
