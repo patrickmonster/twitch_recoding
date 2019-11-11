f9822f7639ad47c85d14a9157d2dc1414aa9dbcd
Soung jin Ryu <rsj11201@gmail.com>
2019-11-12 01:29:51 +0900

    TwitchWSS.class
    클래스화 시킴

--- chatbot.py
+++ chatbot.py
@@ -5,110 +5,82 @@ import websocket
 import requests
 
 
-channel = '#' # 접속 채팅방 이름 >#userid
-username = ''#챗봇 클라이언트 id
-passwd = 'oauth:'#리다이렉션 url
-comm = "!" #봇 명령 인식 기호
+class TwitchWSS(websocket.WebSocketApp):
 
-def on_message(ws, message):
-    if message == "":
-        return
-    #try:
-    data = parse_message(message.replace('\r\n',''))
-    if data['command'] == "PRIVMSG":
-        if data['message'][0] == comm:
-            get_command(ws,data,data['message'][1:])
-        else:
-            get_message(ws,data,data['message'])
-    elif data['command'] == "PING":
-        on_send(ws,data['message'],"PONG")
-    elif data['command'] == "JOIN":
-        print(data['message'])
+    def __init__(self,username,passwd,channel,on_message,on_command=None,command="!",onError=None):
+        super().__init__("wss://irc-ws.chat.twitch.tv:443/",on_open=self.onOpen,on_message = self.onMessage,on_error = onError,on_close = self.onClose)
+        self.username = username
+        self.passwd = passwd
+        self.channel = channel
 
-def on_error(ws, error):
-    print("ERROR", error)
+        self._message = on_message
+        self.command = command
+        self.oncommand = on_command
 
-def on_close(ws):
-    try:
-        ws.closed()
-    except:
-        pass
-    print("### closed ###")
 
-def send_message(ws, message):
-    on_send(ws,channel + " :" +str(message))
 
-def on_send(ws,message ,option ='PRIVMSG'):
-    #print("전송 : %s %s" %(option,message))
-    ws.send(option + " " + message)
+    def onClose(self,ws):
+        try:
+            ws.closed()
+        except:
+            pass
+        print("### closed ###")
 
-def on_open(ws):
-    on_send(ws,"REQ :twitch.tv/tags twitch.tv/commands twitch.tv/membership","CAP")
-    on_send(ws,passwd,"PASS")
-    on_send(ws,username,"NICK")
-    on_send(ws,channel,"JOIN")
+    def onOpen(self,ws):
+        self.onSend(ws,"REQ :twitch.tv/tags twitch.tv/commands twitch.tv/membership","CAP")
+        self.onSend(ws,passwd,"PASS")
+        self.onSend(ws,username,"NICK")
+        self.onSend(ws,channel,"JOIN")
+    
+    def sendMessage(self,ws, message):
+        self.onSend(ws,channel + " :" +str(message))
 
-def parse_message(rawMessage):
-    parseMessage = {'message':'','command':''}
-    if (rawMessage[0] == ':'):
-        data = rawMessage.split()
-        parseMessage['command'] = data[1]
-        if (parseMessage['command'] == 'JOIN'):
-            parseMessage['message'] = "Join user :" + data[2]
-        else :
-            parseMessage['message'] = rawMessage
-    elif rawMessage[:4] in ["PING"]:
-        parseMessage['command'],parseMessage['message'] = rawMessage.split()
-    else:
-        data = rawMessage.split(";")
-        for i in data:
-            d = i.split("=")
-            if i == '' or len(d) < 2:
-                continue
-            parseMessage[d[0]] = d[1]
-        if 'user-type' in parseMessage:
-            user_type = parseMessage['user-type'].split()
-            if "mod" in user_type[0]: #타 봇/관리자 
-                del user_type[0]
-            parseMessage['user-connect'] = user_type[0]
-            parseMessage['command'] = user_type[1]
-            parseMessage['chatroom'] = user_type[2]
-            parseMessage['message'] = " ".join(user_type[3:])[1:]
-    return parseMessage
+    def onSend(self,ws,message ,option ='PRIVMSG'):
+        ws.send(option + " " + message)
 
-countl = {}
-
-def get_command(ws,data,message):
-    if len(message) == 0:
-        return
-    message = message.split()
-    #print(message)
-    if message[0] == "생존":
-        send_message(ws,"Recoding bot 실행중...")
-    elif message[0] == "카운트":
-        if len(message) <= 1:
-            send_message(ws, "명령에러] :!카운트 <카운트이름>")
+    def onMessage(self,ws, message):
+        if message == "":
             return
-        if message[1] not in countl:
-            countl[message[1]] = 0
-            send_message(ws, "항목 추가 "+ message[1])
-        else:
-            send_message(ws,"현재 카운트 :" + str(countl[message[1]]))
-    else:
-        #send_message(ws,"명령어 추가중..")
-        return
-def get_message(ws,data,message):
-    print(data['display-name']+" :",data['message'])
-    for i in countl:
-        if i in message:
-            countl[i] += 1
-            break
+        data = self.parse_message(message.replace('\r\n',''))
+        if data['command'] == "PRIVMSG":
+            if data['message'][0] == comm:
+                if callable(self.oncommand):
+                    self.oncommand(ws,data,data['message'][1:])
+            else:
+                if callable(self.oncommand):
+                    self._message(ws,data,data['message'])
+        elif data['command'] == "PING":
+            self.onSend(ws,data['message'],"PONG")
+        elif data['command'] == "JOIN":
+            print(data['message'])
 
-if __name__ == "__main__":
-    #websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("wss://irc-ws.chat.twitch.tv:443/",
-                            on_open = on_open,
-                            on_message = on_message,
-                            on_error = on_error,
-                            on_close = on_close)
-    ws.run_forever()
+    def run(self):
+        super().run_forever()
+
+    def parse_message(self,rawMessage):# 구문분석
+        parseMessage = {'message':'','command':''}
+        if (rawMessage[0] == ':'):
+            data = rawMessage.split()
+            parseMessage['command'] = data[1]
+            if (parseMessage['command'] == 'JOIN'):
+                parseMessage['message'] = "Join user :" + data[2]
+            else :
+                parseMessage['message'] = rawMessage
+        elif rawMessage[:4] in ["PING"]:
+            parseMessage['command'],parseMessage['message'] = rawMessage.split()
+        else:
+            data = rawMessage.split(";")
+            for i in data:
+                d = i.split("=")
+                if i == '' or len(d) < 2:
+                    continue
+                parseMessage[d[0]] = d[1]
+            if 'user-type' in parseMessage:
+                user_type = parseMessage['user-type'].split()
+                if "mod" in user_type[0]:
+                    del user_type[0]
+                parseMessage['user-connect'] = user_type[0]
+                parseMessage['command'] = user_type[1]
+                parseMessage['chatroom'] = user_type[2]
+                parseMessage['message'] = " ".join(user_type[3:])[1:]
+        return parseMessage
