031d25c0698d2259f82dcdc39ded37d67a4fd269
Soung jin Ryu <rsj11201@gmail.com>
2019-11-19 13:18:29 +0900

    Update chatbot.py
    실제 사용자명령의 처리 가능토록 제작
    스트리머의 방송 시작 시간값 추가
    트위치 API 값 불러오기
    시간 연산 기능 추가
    동작시, 자동으로 스트리머 방송시작 시간 불러옴

--- chatbot.py
+++ chatbot.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
-import os
+import time
 import json
-import websocket
 import requests
+import websocket
+'''
+데이터 생성 규칙
+tmi-sent-ts     // 서버에 메세지 전송된 시각
+message         // 메세지
+
 
 '''
-project by Soungjin
-make 2019.11.02
-'''
+
+import datetime
 
 class TwitchWSS(websocket.WebSocketApp):
 
-    def __init__(self,username,passwd,channel,on_message,on_command=None,command="!",onError=None):
+    def __init__(self,username,passwd,channel,on_message,on_command=None,command="!",onError=None,on_sys_command=None):
         super().__init__("wss://irc-ws.chat.twitch.tv:443/",on_open=self.onOpen,on_message = self.onMessage,on_error = onError,on_close = self.onClose)
         self.username = username
         self.passwd = passwd
@@ -20,6 +24,20 @@ class TwitchWSS(websocket.WebSocketApp):
         self._message = on_message
         self.command = command
         self.oncommand = on_command
+        self.onsyscommand = on_sys_command
+
+        self.start_time = None
+
+        self.header = {'Client-ID': username, 'Accept': 'application/vnd.twitchtv.v5+json'}
+
+        self.streamer = self.get_api_request("kraken/users?login="+channel[1:])['users'][0]
+
+        print("방송시간 :",self.convert_time(self.get_stream_running_s()))
+
+    def convert_time(self,s):
+        if s <= 0:
+            return "방송중이 아닙니다!"
+        return time.strftime("%H시%M분%S초", time.gmtime(s))
 
     def onError(self,message):
         print("Error",message)
@@ -37,8 +55,10 @@ class TwitchWSS(websocket.WebSocketApp):
         self.onSend(self.passwd,"PASS")
         self.onSend(self.username,"NICK")
         self.onSend(self.channel,"JOIN")
+        self.start_timev = time.strftime("%H시%M분", time.gmtime(s))
     
     def sendMessage(self, message):
+        print("메세지 전송",message)
         self.onSend(self.channel + " :" +str(message))
 
     def onSend(self,message ,option ='PRIVMSG'):
@@ -51,17 +71,21 @@ class TwitchWSS(websocket.WebSocketApp):
         if data['command'] == "PRIVMSG":
             if data['message'][0] == self.command:
                 if self.oncommand:
+                    print(self,data['message'][1:])
                     self.oncommand(self,data,data['message'][1:])
             else:
                 if self._message:
+                    #print(data['display-name']+" :",data['message'])
                     self._message(self,data,data['message'])
         elif data['command'] == "PING":
             self.onSend(data['message'],"PONG")
         elif data['command'] == "JOIN":
             print(data['message'])
+        else :
+            if self.onsyscommand:# 기타처리
+                self.onsyscommand(data)
 
     def run(self):
-        print("run")
         super().run_forever()
 
     def parse_message(self,rawMessage):# 구문분석
@@ -86,8 +110,27 @@ class TwitchWSS(websocket.WebSocketApp):
                 user_type = parseMessage['user-type'].split()
                 if "mod" in user_type[0]:
                     del user_type[0]
+                    parseMessage['user-mod'] = True
+                else :
+                    parseMessage['user-mod'] = False
                 parseMessage['user-connect'] = user_type[0]
                 parseMessage['command'] = user_type[1]
                 parseMessage['chatroom'] = user_type[2]
                 parseMessage['message'] = " ".join(user_type[3:])[1:]
         return parseMessage
+
+    def get_api_request(self,url):
+        return json.loads(requests.get("https://api.twitch.tv/"+url,headers=self.header).content.decode("UTF-8"))
+
+    def get_stream_started(self):
+        try:
+            # 한국시 보정 + 10
+            return datetime.datetime.strptime(self.get_api_request("helix/streams?user_id=" + self.streamer['_id'])['data'][0]['started_at'], '%Y-%m-%dT%H:%M:%SZ') + datetime.timedelta(hours=9)
+        except :
+            return datetime.datetime.now()
+    
+    def get_stream_running_s(self):
+        return self.get_passing_time(self.get_stream_started()).seconds
+
+    def get_passing_time(self,time):
+        return datetime.datetime.now()  - time
